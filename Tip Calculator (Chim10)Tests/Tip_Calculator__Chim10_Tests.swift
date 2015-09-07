8ac91e083cc553467a9bd9c922efb7521a095309
chimten <tim.chen57@gmail.com>
2015-09-06 23:05:07 -0600

    Initial Commit

---
+++ Tip Calculator (Chim10)Tests/Tip_Calculator__Chim10_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Tip_Calculator__Chim10_Tests.swift
+//  Tip Calculator (Chim10)Tests
+//
+//  Created by Tim Chen on 9/6/15.
+//  Copyright (c) 2015 Tim Chen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Tip_Calculator__Chim10_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
